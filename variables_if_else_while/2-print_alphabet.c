b069439acc2db8d0c3aa2469c54ea53872f7e096
Jessf84 <14909@holbertonstudents.com>
2026-07-08 05:53:29 +0000

    Add print alphabet and put char script

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,26 @@
 #include <unistd.h>
 
 /**
- * main - prints the alphabet in lowercase
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase using _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
- char ch = 'a';
+ char ch;
 
- while (ch <= 'z')
+ /* The prototype for the custom _putchar function */
+ int _putchar(char c);
+
+ /* First use of _putchar: inside the loop to print the alphabet */
+ for (ch = 'a'; ch <= 'z'; ch++)
  {
-  write(1, &ch, 1);
-  ch++;
+  _putchar(ch);
  }
 
+ /* Second use of _putchar: to print the newline character */
  _putchar('\n');
 
  return (0);
