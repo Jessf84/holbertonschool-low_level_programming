2e6bf4bc4fdff81bcc27ce1f3f8aad8e837af95d
Jessf84 <14909@holbertonstudents.com>
2026-07-08 05:06:50 +0000

    Add print alphabet script

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,31 +1,22 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- */
-int _putchar(char c)
-{
- return (write(1, &c, 1));
-}
-
-/**
  * main - prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
-{
-		_putchar(ch);
+ char ch = 'a';
+
+ while (ch <= 'z')
+ {
+  write(1, &ch, 1);
+  ch++;
+ }
+
+ _putchar('\n');
+
+ return (0);
 }
-	
-	_putchar('\n');
-	
-	return (0);
-}
+
