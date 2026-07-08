97ff133932ce9e340715920060e1fbbd6b9d8b4f
Jessf84 <14909@holbertonstudents.com>
2026-07-08 04:51:43 +0000

    Add print alphabet script

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+{
+		_putchar(letter);
+}
+	
+	_putchar('\n');
+	
+	return (0);
+}
+
