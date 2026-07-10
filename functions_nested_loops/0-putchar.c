57f2adab177301dd3a979724b3f00c8a492db163
Jessf84 <14909@holbertonstudents.com>
2026-07-10 08:48:01 +0000

    Add task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - Prints _putchar followed by a new line.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	putchar(str[i]);
+	i++;
+	}
+	putchar('\n');
 
 	return (0);
 }
