202ecddb37c689078aa26af1f202e1ef9828df28
Jessf84 <14909@holbertonstudents.com>
2026-07-15 08:40:40 +0000

    add task 3 puts

---
+++ pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Returne: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lackof them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
+
