47f820e927a7b22cb52267c085a5f2c1fc340cbf
Jessf84 <14909@holbertonstudents.com>
2026-07-22 01:02:17 +0000

    Task 1

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -5,5 +5,9 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 
 #endif
