19b1d08f5bcf830daea02e04ef5db71f802d4124
Jessf84 <14909@holbertonstudents.com>
2026-07-23 07:46:12 +0000

    Task 0 Simple Calculator

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - shows a simple calculator menu
+ *
+ * Return: Alway 0
+ */
+int main(void)
+{
+	int choice;
+
+	printf("Simple Calculator\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Wuit\n");
+	printf("Choice: ");
+
+
+	scanf("%d", &choice);
+
+	if (choice == 0)
+		printf("Bye!\n");
+
+	return (0);
+}
