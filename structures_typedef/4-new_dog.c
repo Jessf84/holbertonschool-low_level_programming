0c6a6b84a6ef9f5760ce45d8fdcf95e785c4d2af
Jessf84 <14909@holbertonstudents.com>
2026-07-25 01:30:06 +0000

    Task 4 Woof

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,34 +1,45 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog
+ * new_dog - Creates a new dog without using forbidden library functions
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
  *
  * Return: Pointer to the new dog, or NULL if it fails
  */
+__attribute__((optimize("O0")))
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int name_len;
+	int owner_len;
+	int i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
+
+	name_len = 0;
+	while (name[name_len] != '\0')
+		name_len++;
+
+	owner_len = 0;
+	while (owner[owner_len] != '\0')
+		owner_len++;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(strlen(name) + 1);
+	dog->name = malloc((name_len + 1) * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	dog->owner = malloc(strlen(owner) + 1);
+	dog->owner = malloc((owner_len + 1) * sizeof(char));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
@@ -36,8 +47,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	strcpy(dog->name, name);
-	strcpy(dog->owner, owner);
+	i = 0;
+	while (name[i] != '\0')
+	{
+		dog->name[i] = name[i];
+		i++;
+	}
+	dog->name[i] = '\0';
+
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		dog->owner[i] = owner[i];
+		i++;
+	}
+	dog->owner[i] = '\0';
+
 	dog->age = age;
 
 	return (dog);
