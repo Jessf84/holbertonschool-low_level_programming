e5efaf6cfcbcbdf09edb7714c4f24941a90ba35f
Jessf84 <14909@holbertonstudents.com>
2026-07-29 23:43:25 +0000

    Task 1 Print

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@
  *
  * Return: Nothing.
  */
-void print_numbers(const car *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
