de72b487824209a8db8a3f80e677951efd9f6ce9
Jessf84 <14909@holbertonstudents.com>
2026-07-29 03:09:50 +0000

    Task 0 Bob

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
 
-/** 
- * print_name - Prints a name using a supplied function
+/**
+ * print_name - Prints a name
  * @name: Name to print
  * @f: Function used to print the name
- * */
+ *
+ * Description: Calls a function to print a name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
