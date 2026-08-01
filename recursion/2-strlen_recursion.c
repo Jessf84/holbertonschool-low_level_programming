3e4af9908899984d849eeea42502b90a77339670
Jessf84 <14909@holbertonstudents.com>
2026-08-01 04:02:56 +0000

    Task2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,18 +2,16 @@
 
 /**
  * _strlen_recursion - Returns the length of a string using recursion.
- * @s: The string to mesure.
+ * @s: The string to measure.
  *
- * Return: The lengh of the string.
+ * Return: The length of the string.
  */
-int _strlen_recurstion(char *s)
+int _strlen_recursion(char *s)
 {
-	/* Base case: If the current character is the nullterminator */
 	if (*s == '\0')
 	{
 		return (0);
 	}
 
-	/* Recursibe step: 1 + length of the rest of the string */
 	return (1 + _strlen_recursion(s + 1));
 }
