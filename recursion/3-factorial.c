ab4206f666616840e8e2424869a7c22ab1e54bad
Jessf84 <14909@holbertonstudents.com>
2026-08-01 04:23:41 +0000

    Task3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find the factorial of.
+ *
+ * Return: The factorial of n, or -1 if n is lower then0.
+ */
+int factorial(int n)
+{
+	/* Error case: Factorial is not defined for negativen numbers */
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+
+	/* Vase case: Factorial of 0 is 1 */
+	if (n ==0)
+	{
+		return (1);
+	}
+
+	/* Recursive step: n * factorial (n - 1) */
+	return (n * factorial(n - 1));
+}
---
+++ recursion/3-main.c
@@ -0,0 +1,22 @@
+ #include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = factorial(1);
+	printf("%d\n", n);
+	n = factorial(5);
+	printf("%d\n", n);
+	n = factorial(10);
+	printf("%d\n", n);
+	n = factorial(-1024);
+	printf("%d\n", n);
+	return (0);
+}
