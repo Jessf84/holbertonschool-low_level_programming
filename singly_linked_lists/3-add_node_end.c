e9073a284e75a14270b7813192a3d7d387e6c13f
Jessf84 <14909@holbertonstudents.com>
2026-08-09 10:22:46 +0000

    Task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+ #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -11,17 +11,14 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *temp;
+	list_t *new_node, *temp;
 	unsigned int len = 0;
 
 	if (head == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	if (str != NULL)
 	{
 		new_node->str = strdup(str);
@@ -30,29 +27,24 @@ list_t *add_node_end(list_t **head, const char *str)
 			free(new_node);
 			return (NULL);
 		}
-		while (str[len] != '\0')
+		while (str[len])
 			len++;
 	}
 	else
 	{
 		new_node->str = NULL;
-		len = 0;
 	}
-
 	new_node->len = len;
 	new_node->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
-
 	temp->next = new_node;
-
 	return (new_node);
 }
+
